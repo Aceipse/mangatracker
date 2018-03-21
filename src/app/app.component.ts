20db3bce940cc859445893e85d276151d1231102
martin nielsen fig <martin.fig@hotmail.com>
2018-03-21 19:19:23 +0100

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  constructor(private http: HttpClient){}
+  public mangas: Array<any>;
+
+  public getData(){
+    this.http.get('http://localhost:64472/api/Tracker').subscribe((data)=>{
+      this.mangas = <Array<any>> data;
+    });
+  }
 }
